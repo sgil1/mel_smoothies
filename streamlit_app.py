dc0327fa50e6a7aed0205bc4583cff756457c1fc
sgil1 <sergio.gil@sdggroup.com>
2025-01-02 14:37:40 +0000

    another typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,4 +33,4 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-sf_df = st.dataframe(data=smoothiefroot_response.json(), user_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
