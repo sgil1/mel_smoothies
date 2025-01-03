8c61f90da3afd1ceafb49cb0a933173d648db67e
sgil1 <sergio.gil@sdggroup.com>
2025-01-03 09:44:40 +0000

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ if ingredients_list:
         st.subheader(fruit_chosen + ' Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         
-        if smoothiefroot_response:
+        if smoothiefroot_response.json():
             sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         else:
             st.write(fruit_chosen + " is not on the database.")
